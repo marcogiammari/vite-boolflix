d6fdc076186f974dadb5f71101bca4480e1a5cd0
Marco Giammari <marcogiammari@gmail.com>
2023-05-25 19:00:23 +0200

    milestone 3

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -15,6 +15,14 @@ export default {
                 return 'fi fi-gb'
             }
             return 'fi fi-' + lang
+        },
+        getImgPath(path) {
+            let fullPath = "https://image.tmdb.org/t/p/w342/" + path;
+            return fullPath
+        },
+        changeInStars(vote) {
+            const stars = Math.ceil(vote / 2);
+            return stars
         }
     }
 }
@@ -23,10 +31,27 @@ export default {
 
 <template>
     <div
-        class="card flex flex-col justify-around text-white bg-slate-900 rounded-xl border border-red-900 min-w-[200px] p-3 w-1/5">
-        <p>Titolo: {{ movie.title }}</p>
-        <p>Titolo originale: {{ movie.original_title }}</p>
-        <p>Lingua: <span :class="getFlag(movie.original_language)"></span></p>
-        <p>Voto: {{ movie.vote_average == '0' ? 'n/a' : movie.vote_average }}</p>
+        class="card flex flex-col justify-start gap-4 overflow-auto text-white bg-slate-900 rounded-xl border border-red-900 min-w-[200px] max-h-[300px] p-4">
+        <div class="h-1/3">
+            <img v-show="movie.backdrop_path != null" class="rounded" :src="getImgPath(movie.backdrop_path)"
+                :alt="movie.title">
+            <p v-show="movie.backdrop_path == null"
+                class="bg-white text-red-600 h-full flex justify-center items-center font-bold text-xl">
+                N/A</p>
+        </div>
+        <div>
+            <p>Titolo: {{ movie.title }}</p>
+            <p>Titolo originale: {{ movie.original_title }}</p>
+            <p>Lingua: <span :class="getFlag(movie.original_language)"></span></p>
+            <p v-if="movie.vote_average > 0">
+                Voto:
+                <span v-for="star in changeInStars(movie.vote_average)"><i
+                        class="fa-solid fa-star text-yellow-400"></i></span>
+                <span v-for="star in 5 - changeInStars(movie.vote_average)"><i
+                        class="fa-regular fa-star text-yellow-400"></i></span>
+            </p>
+            <p v-else>Voto : n/a</p>
+
+        </div>
     </div>
 </template>
--- src/components/AppSeries.vue
+++ src/components/AppSeries.vue
@@ -15,6 +15,16 @@ export default {
                 return 'fi fi-gb'
             }
             return 'fi fi-' + lang
+        },
+        getImgPath(path) {
+            let fullPath = "https://image.tmdb.org/t/p/w342/" + path;
+            return fullPath
+        },
+        changeInStars(vote) {
+            if (vote == 0) {
+                return 'n/a'
+            }
+            return Math.ceil(vote / 2)
         }
     }
 }
@@ -23,10 +33,20 @@ export default {
 
 <template>
     <div
-        class="card flex flex-col justify-around text-white bg-slate-900 rounded-xl border border-red-900 min-w-[200px] p-3 w-1/5">
-        <p>Titolo: {{ serie.name }}</p>
-        <p>Titolo originale: {{ serie.original_name }}</p>
-        <p>Lingua: <span :class="getFlag(serie.original_language)"></span></p>
-        <p>Voto: {{ serie.vote_average == '0' ? 'n/a' : serie.vote_average }}</p>
+        class="card flex flex-col justify-start gap-4 overflow-auto text-white bg-slate-900 rounded-xl border border-red-900 min-w-[200px] max-h-[300px] p-4">
+        <div class="h-1/3">
+            <img v-show="serie.backdrop_path != null" :src="getImgPath(serie.backdrop_path)" :alt="serie.name">
+            <p v-show="serie.backdrop_path == null"
+                class="bg-white text-red-600 h-full flex justify-center items-center font-bold text-xl">
+                N/A</p>
+        </div>
+        <div>
+            <p>Titolo: {{ serie.name }}</p>
+            <p>Titolo originale: {{ serie.original_name }}</p>
+            <p>Lingua: <span :class="getFlag(serie.original_language)"></span></p>
+            <p>Voto: <span v-for="star in changeInStars(serie.vote_average)"><i
+                        class="fa-solid fa-star text-yellow-400"></i></span></p>
+        </div>
+
     </div>
 </template>
