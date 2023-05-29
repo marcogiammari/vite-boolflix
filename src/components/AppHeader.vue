8a489c9ed66e5834bc616475b345546a28046f4b
Marco Giammari <marcogiammari@gmail.com>
2023-05-29 12:29:34 +0200

    style(appheader) filter button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,8 @@ export default {
       movieGenre: "",
       seriesGenre: "",
       query: "",
-      store
+      store,
+      filter: false,
     }
   }
 }
@@ -18,27 +19,31 @@ export default {
     <nav class="p-5 mx-5 flex flex-col md:flex-row justify-between align-middle gap-3">
       <h1 class="text-red-500 font-bold text-4xl">BOOLFLIX</h1>
       <div class="flex align-middle gap-3">
-        <select v-model="movieGenre" name="" id="">
-          <option disabled value="">
-            Filtra i film per genere
-          </option>
-          <option v-for="mGenre in this.store.movieGenres" :value="mGenre">
-            {{ mGenre.name }}
-          </option>
-        </select>
-        <select v-model="seriesGenre" name="" id="">
-          <option disabled value="">
-            Filtra le serie TV per genere
-          </option>
-          <option v-for="sGenre in this.store.seriesGenres" :value="sGenre">
-            {{ sGenre.name }}
-          </option>
-        </select>
+        <button @click="filter = !filter" class="text-red-400 p-3 font-bold rounded border">Filtra per genere</button>
+        <div class="flex flex-col gap-1" v-show="filter">
+          <select v-model="movieGenre" name="" id="">
+            <option disabled value="">
+              Filtra i film per genere
+            </option>
+            <option v-for="mGenre in this.store.movieGenres" :value="mGenre">
+              {{ mGenre.name }}
+            </option>
+          </select>
+          <select v-model="seriesGenre" name="" id="">
+            <option disabled value="">
+              Filtra le serie TV per genere
+            </option>
+            <option v-for="sGenre in this.store.seriesGenres" :value="sGenre">
+              {{ sGenre.name }}
+            </option>
+          </select>
+        </div>
         <input class="p-2" type="text" v-model="query"
           @keyup.enter="$emit('getMovies', this.movieGenre, this.query, this.store.searchMoviesAPI), $emit('getSeries', this.seriesGenre, this.query, this.store.searchSeriesAPI)">
         <button
           @click="$emit('getMovies', this.movieGenre, this.query, this.store.searchMoviesAPI), $emit('getSeries', this.seriesGenre, this.query, this.store.searchSeriesAPI)"
-          class="text-red-500 p-3 font-bold rounded border">Search</button>
+          class="text-red-400 p-3 font-bold rounded border">Search
+        </button>
       </div>
     </nav>
   </header>
